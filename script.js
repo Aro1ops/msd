0203b0889a25f952823c26e5aecae65ff6e47680
Aro1ops <127445285+Aro1ops@users.noreply.github.com>
2024-08-31 14:21:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -162,6 +162,15 @@ document.addEventListener("DOMContentLoaded", () => {
         updateEnergyBar();
     });
 
-    // Add clicks
+    document.addEventListener('touchstart', function(event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        }
+    }, { passive: false });
+
+    document.addEventListener('dblclick', function(event) {
+        event.preventDefault();
+    }, { passive: false });
+});// Add clicks
 
 });
